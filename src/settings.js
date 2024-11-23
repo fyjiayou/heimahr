08abc3ab594cd9ed1d4df6e9764975548d8ef1e9
fengyue <fystart@163.com>
2024-11-23 23:45:55 +0800

    加入教学初始化资源

--- src/settings.js
+++ src/settings.js
@@ -1,14 +1,19 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  // 网站标题
+  title: '人力资源管理后台',
 
   /**
+   * 是否固定头部组件
+   *
    * @type {boolean} true | false
    * @description Whether fix the header
    */
-  fixedHeader: false,
+  fixedHeader: true,
 
   /**
+   * 是否显示侧边栏logo
+   *
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
