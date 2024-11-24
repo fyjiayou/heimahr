d3871e899c499dc075453895472b2146eba77165
fengyue <fystart@163.com>
2024-11-24 20:32:07 +0800

    显示用户信息

--- src/store/getters.js
+++ src/store/getters.js
@@ -2,8 +2,8 @@ const getters = {
   sidebar: state => state.app.sidebar,
   device: state => state.app.device,
   token: state => state.user.token,
-  avatar: state => state.user.avatar,
-  name: state => state.user.name,
+  avatar: state => state.user.userInfo.staffPhoto, // 向外暴露头像
+  name: state => state.user.userInfo.username, // 向外暴露名称
   userId: state => state.user.userInfo.userId
 }
 export default getters
